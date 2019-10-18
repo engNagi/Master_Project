79ce3dccb42d85afc0dc5829ce7590415ab18c91
engNagi <eng.m.nagi@gmail.com>
2019-10-18 18:50:09 +0200

    local-testing branch

--- Environment.py
+++ Environment.py
@@ -1,5 +1,6 @@
-#TODO   make it much easier to handle
-#TODO   extend the Controller class of ai2thor
+# TODO   make it much easier to handle
+# TODO   extend the Controller class of ai2thor
+# TODO generalize the position of the thirdPartyCamera to all environment
 
 
 from ai2thor.controller import Controller
@@ -35,19 +36,24 @@ class Environment(object):
             event = controller.step(dict(action="ToggleMapView"))
 
     @classmethod
-    def reset(cls, controller, grid_size=0.25, depth_image=False, class_image=False, object_image=False,
+    def reset(cls, controller, grid_size=0.25, scene="FloorPlan220", depth_image=False, class_image=False, object_image=False,
               visibility_distance=1.5, camera_Y=0.675, fov=60.0, object_name="LightSwitch"):
+
+        controller.reset(scene_name=scene)
+
+        controller.step(dict(action="ToggleMapView"))
 
         event = controller.step(dict(action="Initialize", gridSize=grid_size, renderDepthImage=depth_image,
                              renderClassImage=class_image, renderObjectImage=object_image,
                              visibilityDistance=visibility_distance, cameraY=camera_Y, fieldOfView=fov))
 
-        #TODO generalize the position of the thirdPartyCamera to all environment
-        third_party_cam = controller.last_event.metadata["thirdPartyCameras"]
-        if len(third_party_cam) == 0:
-            event = controller.step(
-                dict(action='AddThirdPartyCamera', rotation=dict(x=90, y=0, z=0),
-                        position=dict(x=-2.847458, y=7.5, z=1.9)))
+        #event = controller.step(dict(action="TeleportFull", x=-4.25, y=0.909619451, z=2.75, rotation=90, horizon=0.0))
+
+        # third_party_cam = controller.last_event.metadata["thirdPartyCameras"]
+        # if len(third_party_cam) == 0:
+        #     event = controller.step(
+        # dict(action='AddThirdPartyCamera', rotation=dict(x=90, y=0, z=0),
+        # position=dict(x=-2.847458, y=7.5, z=1.9)))
 
         objects = event.metadata["objects"]
         for obj in objects:
@@ -56,15 +62,16 @@ class Environment(object):
         object_position = obj["position"]
         agent_obj_distace = obj["distance"]
         obs = event.third_party_camera_frames
-        frame = np.squeeze(obs, axis=0)
+        #_ = np.squeeze(obs, axis=0)
         done = event.metadata["lastActionSuccess"]
         agent_position = list(event.metadata["agent"]["position"].values())
         camera_pose = event.metadata["cameraPosition"]
         # agent_rotation = np.array(list(event.metadata["agent"]["rotation"].values()), dtype=float)
         # object_position = list(obj["position"].values())
         # agent_pose = np.concatenate((agent_position, agent_rotation), axis=0)
-        goal = [-0.075, 0.909619451, 1.75]
-        return frame, agent_position, done, goal, object_position
+        goal = [-0.5, 0.909619451, 1.9]
+        return agent_position, done, goal, object_position
+
 
     @classmethod
     def take_action(cls, action, controller, object_name="LightSwitch"):
@@ -159,18 +166,19 @@ class Environment(object):
 
         # obs = event.frame
         obs = event.third_party_camera_frames
-        frame = np.squeeze(obs, axis=0)
-        done = event.metadata["lastActionSuccess"]
+        #_ = np.squeeze(obs, axis=0)
+        done = not event.metadata["lastActionSuccess"]
         agent_position = list(event.metadata["agent"]["position"].values())
         agent_rotation = list(event.metadata["agent"]["rotation"].values())
         # agent_pose = np.concatenate((agent_position, agent_rotation), axis=0)
 
-        return frame, agent_position, done, reward, obj_agent_dis, visible
+        return agent_position, done, reward, obj_agent_dis, visible
 
-    @classmethod
-    def reward(cls, obj_agent_dis_, obj_agent_dis):
-        if obj_agent_dis_ < obj_agent_dis:
-            reward = 1
-        else:
-            reward = 0
-        return reward
+
+@classmethod
+def reward(cls, obj_agent_dis_, obj_agent_dis):
+    if obj_agent_dis_ < obj_agent_dis:
+        reward = 1
+    else:
+        reward = 0
+    return reward
