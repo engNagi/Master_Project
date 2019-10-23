603a6a6e84c84bce2ee10dd33d8c6cc067bd9ebc
engNagi <eng.m.nagi@gmail.com>
2019-10-23 16:23:26 +0200

    Environment working

--- env.py
+++ env.py
@@ -1,26 +1,16 @@
-from Environment import Environment as env
+from Environment import Environment
 import numpy as np
-import ai2thor.controller
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 
-cl = ai2thor.controller.Controller()
-# cl.start()
-# # can be any one of the scenes FloorPlan###
-# #LightSwitch
-# cl.reset('FloorPlan28')
-# event = cl.step(dict(action='Initialize', gridSize=0.25))
+env = Environment(top_view_cam=False)
 
-env.make(controller=cl)
-
-agent_pose, done, object_position, _ = env.reset(controller=cl, grid_size=0.15)
-print("agent_position:", agent_pose, "object position:", object_position)
+env.make()
+first_person_obs, agent_position, goal, _, _ = env.reset()
 
 done = False
 while not done:
     action = np.random.randint(0, 5)
-    agent_pose_, done, reward, obj_agent_dis, visible, _ = env.take_action(action, controller=cl, object_name="Television")
-    print("visible:", visible, "\n agent_pose:", agent_pose_, "\n done:", done, "\n reward:", reward,
-          "\n distance:", obj_agent_dis)
-    # plt.imshow(frame)
-    # plt.show()
+    frame, agent_position, done, reward = env.take_action(action)
+    print("agent_pose:", agent_position, "\n done:", done, "goal:", goal)
+    plt.imshow(frame)
+    plt.show()
